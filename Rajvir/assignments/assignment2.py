d7fc5c2ffbf6ff64c81649573549f5c0f332e925
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-02-15 14:47:07 +0300

    adding all my work to my branch

---
+++ Rajvir/assignments/assignment2.py
@@ -0,0 +1,9 @@
+## creating a constant and printing it......
+# A constant is a special type of variable whose value cannot be changed. they are usuaslly declared and 
+# assigned as a module 
+
+#declare 
+constantPI = 3.14
+constantGRAVITY = 9.8
+
+print(constant PI)
