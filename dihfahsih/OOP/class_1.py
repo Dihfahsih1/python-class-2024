9ab13f6fe0da4a915c2da8e3f07244cc074ab499
Dihfahsih1 <dihfahsihm@gmail.com>
2024-02-18 23:45:46 +0300

    fix the errors in the code

--- dihfahsih/OOP/class_1.py
+++ dihfahsih/OOP/class_1.py
@@ -1,12 +1,13 @@
 class Human:
     def __init__(self, name, age, gender):
-        self.age=age
-        self.name=name 
-        self.gender=gender
+        self.name = name
+        self.age = age
+        self.gender = gender
         
     def introduce(self):
-        print(f"Hello, my name is {self.name}, i'm {self.age} years old and my gender is {self.gender} ")
-#creating objects    
+        print(f"Hello, my name is {self.name}, I'm {self.age} years old and my gender is {self.gender}.")
+
+# Creating objects    
 person1 = Human('Alice', 30, 'Female')
 person2 = Human('Ali', 20, 'Male')
 
@@ -14,8 +15,13 @@ person1.introduce()
 person2.introduce()
 
 class Man(Human):
-    def __init__(self, college, weight):
-        super().__init__(self)
-        self.age=age
+    def __init__(self, name, age, gender, college, weight):
+        super().__init__(name, age, gender)
+        self.college = college
+        self.weight = weight
 
-    
+# Creating a Man object
+man1 = Man('John', 25, 'Male', 'XYZ College', 75)
+
+# Accessing attributes of the Man object
+print(f"{man1.name} attends {man1.college} and weighs {man1.weight} kgs.")
