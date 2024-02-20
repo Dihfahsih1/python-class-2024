8c3425a91c071a13044ab77f462cc9ce697b9de9
tmaurice13 <mauricetabaruka@gmail.com>
2024-02-20 03:02:41 -0800

    maurice

--- maurice/PROJECT/weather.py
+++ maurice/PROJECT/weather.py
@@ -1,11 +1,11 @@
-import requests
+# import requests
 
-api_key='5ecbc1c45b756f07aa95e6bac961f916'
+# api_key='5ecbc1c45b756f07aa95e6bac961f916'
 
-user_input=input("Enter city: ")
+# user_input=input("Enter city: ")
 
-weather_data=requests.get(
-    f'https//api.weathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}')
-weather=weather_data.json()['weather'][0]['main']
-temp=(weather_data.json()['main']['temp'])
-print(weather,temp)
+# weather_data=requests.get(
+#     f'https//api.weathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}')
+# weather=weather_data.json()['weather'][0]['main']
+# temp=(weather_data.json()['main']['temp'])
+# print(weather,temp)
