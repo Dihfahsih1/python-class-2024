0cf2b270b2a5e25591a89461080d20774dd438b8
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-02-22 18:20:39 +0300

    new

---
+++ Rajvir/advanced_func/range.py
@@ -0,0 +1,5 @@
+start= int(input("Enter the range you want to begin with: "))
+end= int(input("Enter the range with a number u want to stop: "))
+step = int(input("Enter the step u want it to follow: "))
+for i in range(start,end,step):
+    print(i)
