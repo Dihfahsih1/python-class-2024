3cb5d1c177965eea91efba45ffebb543ea32aea6
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-02-22 13:06:26 +0300

    added work

--- Rajvir/assignments/assignment13/encap.py
+++ Rajvir/assignments/assignment13/encap.py
@@ -1,9 +1,33 @@
-class Shape:
-    def __init__(self, base, height):
-        self.__area = (base*height)/2
-        
-    def get_area(self):
-        return self.__area
+#ENCAPSULATION
+#this is used to restrict access of data members and functions in a class..
 
-triangle= Shape(2, 4)
-print(triangle.get_area())
+class Parent:
+    def __init__(self,name,age):
+        self.__name= name
+        self.__age= age 
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self,name):
+        self.__name = name
+
+    def get_age(self,age):
+        return self.__age
+
+    def set_age(self,age):
+        if age >= 0:
+            self.__age = age
+        else: 
+            print("age cant be negative man..")
+
+class Child(Parent):
+    def __init__(self,name,age,mname):
+        super().__init__(name,age)
+        self.mname = mname
+
+parent1 =Parent("Alice", 20)
+print ("parents name is " , Parent.get_name())
+
+child1 =Child("John", 20, "jack")
+print ("childs name is " , Child.get_name())
