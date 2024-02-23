49abf86e5449d15e2a65a47ed367cd5c0bcd0193
Dihfahsih1 <dihfahsihm@gmail.com>
2024-02-23 15:39:03 +0300

    advanced

--- dihfahsih/advanced_functions/first_module.py
+++ dihfahsih/advanced_functions/first_module.py
@@ -1,12 +1,3 @@
-# __name__ this is a special built in variable in python that represents 
-# the name of the current module when a python script is executed
-
-# Why do we use if __name__ == "__main__"
-# 1. It allows us to separate the code that should be run only when the script is executed  directly
-# 2. It also checks if the script is imported as a module in another module.
-
-#__name__ is a special python variable we use when interacting with modules
-#-- It allows us to make a distinction between modules we import and those we don't
 
 #__main__ is a value that represents the top level environment where we run our code
 def main():
