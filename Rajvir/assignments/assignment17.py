f2dc0a4bb3c5f553ad7aed8f6d647419994dc9de
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-02-27 17:46:35 +0300

    homework

---
+++ Rajvir/assignments/assignment17.py
@@ -0,0 +1,18 @@
+def wr_file():
+    data =input("enter the name of the txt file: ")
+
+    file=open("data.txt","w") 
+    file.write(input("Enter any text here: "))
+    print(file.write)
+    file.close()
+
+    file=open("data.txt","r")
+    content =file.read()
+    print(content)
+    file.close()
+
+    text = input("enter the text to append: ")
+    file=open("data.txt","a")
+    file.write(textg)
+    file.close()
+wr_file()
