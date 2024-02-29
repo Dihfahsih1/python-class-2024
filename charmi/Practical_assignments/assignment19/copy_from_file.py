6ce8434c1a26dea4e3857559c9f1e2f0cf531001
CharmiPanchal17 <charmipanchal17@gmail.com>
2024-02-29 21:50:33 +0300

    assign20

--- charmi/Practical_assignments/assignment19/copy_from_file.py
+++ charmi/Practical_assignments/assignment19/copy_from_file.py
@@ -2,7 +2,7 @@ import shutil
 
 def copy_file():
 
-    source_file = 'C:/Users/Dell/Desktop/python-class-2024/charmi/Practical_assignments/assignment19/assignment_files'
+    source_file = 'assignment_files'
     destination_file = "C:/Users/Dell/Desktop/python-class-2024/charmi/file_handling/files"
 
     try:
--- charmi/Practical_assignments/assignment20/sqrt.py
+++ charmi/Practical_assignments/assignment20/sqrt.py
@@ -2,8 +2,16 @@ import math
 
 #using the math module
 
-square_root = lambda x: x**0.5
+square_root = lambda x: math.sqrt(x)
 
 x = int(input("Enter the number to find the square root of: "))
 
-print("The square root is: ", square_root)
+print("The square root is: ", square_root(x))      #remember to add the parameter x in ()
+
+#using a formula
+
+sqr_root = lambda num: num**(0.5)
+
+num= int(input("Enter the number to find the square root of: "))
+
+print(f"The asnwer is: {sqr_root(num)}")
