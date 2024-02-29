7dda0c2ac7c7d5555f5e4b032b63328abb147fa0
CharmiPanchal17 <charmipanchal17@gmail.com>
2024-02-29 21:35:03 +0300

    ass19

--- charmi/Practical_assignments/assignment19/copy_from_file.py
+++ charmi/Practical_assignments/assignment19/copy_from_file.py
@@ -5,9 +5,11 @@ def copy_file():
     source_file = 'C:/Users/Dell/Desktop/python-class-2024/charmi/Practical_assignments/assignment19/assignment_files'
     destination_file = "C:/Users/Dell/Desktop/python-class-2024/charmi/file_handling/files"
 
-try:
-    shutil.copy(source_file, destination_file)
-    print("File has been copied")
+    try:
+        shutil.copy(source_file, destination_file)
+        print("File has been copied")
 
-except Exception as e:
-    print(F"{e}")
+    except Exception as e:
+        print(F"{e}")
+
+copy_file()
