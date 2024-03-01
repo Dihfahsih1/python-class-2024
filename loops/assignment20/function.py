b1ad8ea704a45d17d15b52aab5bf3e81147b4280
kyayonka1 <joelkyayonka@gmail.com>
2024-03-01 13:50:23 +0300

    work

--- loops/assignment20/function.py
+++ loops/assignment20/function.py
@@ -1,1 +1,11 @@
-numbers = lambda x,y: (x,y)
+# Define the anonymous function to calculate square root
+sqrt = lambda x: x ** 0.4
+
+# Prompt user to input a number
+number = float(input("Enter a number: "))
+
+# Calculate the square root using the anonymous function
+result = sqrt(number)
+
+# Display the result
+print("The square root of", number, "is", result )
