101d7554af05893102393290d61e747df821c928
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-03-01 11:44:06 +0300

    entery of ass 20, ass 18 needs updates

---
+++ Rajvir/assignments/assignment20/squareroot_lambda.py
@@ -0,0 +1,5 @@
+import math
+
+squareroot = lambda a : math.sqrt(a)
+result = squareroot(int(input("Enter a value: ")))
+print(result)
--- Rajvir/functions/measure_time.py
+++ Rajvir/functions/measure_time.py
@@ -1,5 +1,14 @@
 import time
-from task import task
+from sleep import task
 
-def measure_time():
-    task = Task()
+def measuring_time():
+    task = task()
+    return task
+
+start_time = time.time()
+measuring_time()
+
+end_time = time.time()
+
+elapsed_time = end_time- start_time
+print("Elapsed time: ", elapsed_time, " seconds")
