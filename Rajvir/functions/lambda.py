43f4dc222a39f816fb087ef29eeb49c876d650e2
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-03-06 15:59:46 +0300

    classwork

--- Rajvir/functions/lambda.py
+++ Rajvir/functions/lambda.py
@@ -1,7 +1,9 @@
-#lambda is a funuction with no name
-
-square= lambda x: x**2
-print(square(5))
+#are defined using a lambda keyword
+square_root= lambda x: x**2
+print(square_root(2))
 
 is_even = lambda x: x%2==0
 print(is_even(8))
+
+reverse = lambda s:s[::-1]
+print(reverse('python'))
