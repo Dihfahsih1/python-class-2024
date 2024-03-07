a34a59487f73aa50955af8f773902024aaba3c22
afodisiac12 <afodisiacmreyesontheprize@gmail.com>
2024-03-06 22:59:03 -0800

    gotchu

---
+++ afod/assignment4/assignment4.py
@@ -0,0 +1,19 @@
+# How do we use comprehension in python
+
+# squares = []
+# for x in range(10):
+#     squares.append(x**2)
+# print(squares)
+
+# how to remove an element using pop 
+
+k= [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+popped_item= k.pop (3)
+print (popped_item)
+print(k)
+
+#deleting in a dictionary 
+
+# countries= {"uganda": "kamapala", "kenya":"Nairobi"}
+# countries.pop("kenya")
+# print(countries)
